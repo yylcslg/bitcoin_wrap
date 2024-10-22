5c1965885ff8cbd20c8604f94d5cc08cc5062824
yunlong <819243476@qq.com>
2024-10-22 18:28:04 +0800

    bitcoin tx

--- src/wrap/common/utils/constantUtils.ts
+++ src/wrap/common/utils/constantUtils.ts
@@ -1,9 +1,18 @@
 /* eslint-disable prettier/prettier */
 import { AddressType } from "../../../core";
 import { ADDRESS_TYPES, CHAINS_MAP, ChainType } from "../constant";
-
+import moment from 'moment-timezone'
 export function getChain(chainType:ChainType){
     return CHAINS_MAP[chainType];
+}
+
+export function getSignTime(format = 'yyyy-MM-dd HH:mm:ss'){
+    let formattedDateTime = moment().utc().format(format)
+    return formattedDateTime
+}
+
+export function sleep(ms:number){
+    return new Promise(resolve=>setTimeout(resolve, ms))
 }
 
 export const getAddressTypeDetail = (addressType : AddressType)=>{
